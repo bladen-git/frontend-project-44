b84918518bbd9ee2cb72176fc5401bb9cdfe3e6c
Ильдар Халиков <ildarkicck@gmail.com>
2022-12-13 13:29:46 +0300

    5th stage

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,6 @@ import readlineSync from "readline-sync";
 
 console.log("node bin/brain-games.js\nWelcome to the Brain Games!");
 
-export const name = readlineSync.question("Your answer: ");
+export const name = readlineSync.question("May I have your name? ");
 
 console.log(`Hello, ${name}!`);
