e132bbe4b1bc75510f731ba7000f3e3cb28a304e
Ильдар Халиков <ildarkicck@gmail.com>
2022-12-18 17:10:56 +0300

    fix: fixed wrong reply

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ export const correctAnswer = () => console.log('Correct!');
 
 export const wrongAnswer = (wrong, correct, name) => {
   console.log(`${wrong} is wrong answer ;(. Correct answer was ${correct}.`);
-  console.log(`Let's try again, ${name}`);
+  console.log(`Let's try again, ${name}!`);
 };
 
 export const win = (name) => console.log(`Congratulations, ${name}!`);
