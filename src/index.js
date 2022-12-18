f037748ff4fe7113d3a46efecb67ff88084cfe6e
Ильдар Халиков <ildarkicck@gmail.com>
2022-12-18 21:58:28 +0300

    fixed: index.js isPrime function fix

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ export const isPrime = (num) => {
   for (let i = 2; i <= Math.round(Math.sqrt(num)); i += 1) {
     if (num % i === 0) {
       result = 'no';
-    }
+    } else result = 'yes';
   }
   return result;
 };
